001fe3d06dca0fed706dd352f913edb8c95f106b
artemiyulyanov <artemiyulyanov.career@gmail.com>
2026-05-29 01:21:40 +0300

    feat: edit distance task solved

---
+++ editDistance.go
@@ -0,0 +1,35 @@
+package main
+
+func MinDistance(word1 string, word2 string) int {
+	n, m := len(word1), len(word2)
+
+	dp := make([][]int, n+1)
+
+	for i := range dp {
+		dp[i] = make([]int, m+1)
+	}
+
+	for i := 0; i <= n; i++ {
+		dp[i][0] = i
+	}
+
+	for j := 0; j <= m; j++ {
+		dp[0][j] = j
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = 1 + min(
+					dp[i-1][j],   // delete
+					dp[i][j-1],   // insert
+					dp[i-1][j-1], // replace
+				)
+			}
+		}
+	}
+
+	return dp[n][m]
+}
